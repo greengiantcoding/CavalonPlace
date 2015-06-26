2c09785d2142bd3387b92360d33852c6fe29bc1f
Joshua Rieland <greengiantcoding@icloud.com>
2015-06-25 22:03:30 -0700

    added meta tag for responsiveness

--- application/views/available_space.php
+++ application/views/available_space.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 	<script src="/assets/js/JQueryLib.js"></script>
